48b959e1a660f8447b96982325cd7c40538e0e75
Paul Crawford <paulcrawford@users.sourceforge.net>
2019-05-02 17:35:04 +0100

    Apply a short wait before main loop
    - Part of dealing with IPMI watchdog errors on too rapid refresh
    
    - Patch from Vitaly Chikunov

--- src/watchdog.c
+++ src/watchdog.c
@@ -479,6 +479,7 @@ int main(int argc, char *const argv[])
 	swait = 50000;
 	twait = (tint * 1000000) - swait;
 
+	xusleep(swait);
 	/* main loop: update after <tint> seconds */
 	while (_running) {
 		wd_action(keep_alive(), repair_bin, NULL);
